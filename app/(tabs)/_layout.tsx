03cc04a757c981b8a7f07f3c6ff332129346fb9f
xiangkaiyuan <xiangkaiyuan@meituan.com>
2025-03-15 18:48:10 +0800

    feat: 配置bottomTab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,33 @@
-import { Tabs } from "expo-router";
+import { Tabs } from "~/components/ui/BottomTabs";
 import { useColorScheme } from "~/lib/useColorScheme";
 import { Ionicons } from "@expo/vector-icons";
 import * as React from "react";
 
 export default function TabsLayout() {
-  const { isDarkColorScheme } = useColorScheme();
-  
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: isDarkColorScheme ? "#ffffff" : "#000000",
-        tabBarInactiveTintColor: isDarkColorScheme ? "#888888" : "#888888",
-        headerShown: false,
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "待办事项",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="list" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: "已完成",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="checkmark-circle" size={size} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+	const { isDarkColorScheme } = useColorScheme();
+
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: isDarkColorScheme ? "#ffffff" : "#000000",
+				// tabBarInactiveTintColor: isDarkColorScheme ? "#888888" : "#888888",
+				// headerShown: false,
+			}}
+		>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: "待办事项",
+					tabBarIcon: () => ({ sfSymbol: "house" }),
+				}}
+			/>
+			<Tabs.Screen
+				name="explore"
+				options={{
+					title: "已完成",
+					tabBarIcon: () => ({ sfSymbol: "person" }),
+				}}
+			/>
+		</Tabs>
+	);
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -52,7 +52,10 @@ export default function RootLayout() {
 	return (
 		<ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
 			<StatusBar style={isDarkColorScheme ? "light" : "dark"} />
-			<Stack />
+			<Stack>
+				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+				<Stack.Screen name="+not-found" />
+			</Stack>
 		</ThemeProvider>
 	);
 }
